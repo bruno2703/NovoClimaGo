4a827cce10aa266b74a9689da55c6e511d85de57
Bruno Lima <brugui2703@gmail.com>
2023-07-10 06:12:21 -0300

    Firebase
    agora recebo as cidades do firebase

--- app/src/main/java/com/example/climago/FragmentsNavBar/LocaisFragment.kt
+++ app/src/main/java/com/example/climago/FragmentsNavBar/LocaisFragment.kt
@@ -1,7 +1,9 @@
 package com.example.climago.FragmentsNavBar
 
+import android.content.ContentValues.TAG
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -9,6 +11,7 @@ import com.example.climago.AdapterLocais
 import com.example.climago.MapsActivity
 import com.example.climago.R
 import com.example.climago.databinding.FragmentLocaisBinding
+import com.google.firebase.firestore.FirebaseFirestore
 
 class LocaisFragment : Fragment(R.layout.fragment_locais) {
 
@@ -25,6 +28,7 @@ class LocaisFragment : Fragment(R.layout.fragment_locais) {
             startActivity(intent)
         }
 
+        CarregarCidades()
     }
 
     private fun initRecyclerView(){
@@ -40,5 +44,26 @@ class LocaisFragment : Fragment(R.layout.fragment_locais) {
     private fun getTemp() = listOf(
         "22°C", "28°C", "15°C", "33°C", "19°C"
     )
+
+
+    //Firebase
+
+    val db = FirebaseFirestore.getInstance()
+
+
+
+private fun CarregarCidades(){
+        db.collection("cidades")
+            .get()
+            .addOnSuccessListener { result ->
+                for (city in result) {
+                    Log.d("minha", "${city.id} => ${city.data}")
+                }
+            }
+            .addOnFailureListener { exception ->
+                Log.w("minha", "Error getting documents.", exception)
+            }
+    }
+
 
 }
